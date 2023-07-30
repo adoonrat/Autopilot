91bffe587ed9f6dbf1d4aa3c16d1a7e265487c7e
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-07-30 11:51:22 +0700

    Update Start-OSDCloud.ps1

--- Start-OSDCloud.ps1
+++ Start-OSDCloud.ps1
@@ -1,4 +1,11 @@
-Start-OSDCloud -OSName 'Windows 10 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -ZTI -Restart
+Start-OSDCloud -OSName 'Windows 10 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -ZTI
+
+mkdir c:\temp
+copy c:\OSDCloud\Logs\*.log c:\temp\ /y
+rmdir /s /q c:\OSDCloud
+rmdir /s /q c:\Drivers
+
+wpeutil reboot
 
 #-OSBuild 22H2 
 
