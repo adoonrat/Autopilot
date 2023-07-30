3668f858bf2c7ecbabfeedbb890ff8b8ae6a1bff
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-07-30 09:06:42 +0700

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -5,4 +5,4 @@ title OSDCloud 0823
 PowerShell -Nol -C Initialize-OSDCloudStartnet
 @ECHO OFF
 ECHO Start-OSDCloud
-powershell Invoke-Expression -Command (Invoke-RestMethod -Uri https://raw.githubusercontent.com/adoonrat/Autopilot/main/Start-OSDCloud.ps1)
+iex (irm "https://raw.githubusercontent.com/adoonrat/Autopilot/main/Start-OSDCloud.ps1")
