0a72e38252e8c9e05d8ad3d5ec058b06e41718e9
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-07-30 12:46:35 +0700

    Update Start-OSDCloud.ps1

--- Start-OSDCloud.ps1
+++ Start-OSDCloud.ps1
@@ -1,10 +1,10 @@
-Start-OSDCloud -OSName 'Windows 10 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -SkipAutopilot -ZTI
+Start-OSDCloud -OSName 'Windows 10 22H2 x64' -OSLanguage en-us -OSEdition Enterprise -ZTI -Restart
 
-if (!(Test-Path c:\temp)){New-Item -ItemType Directory -Force -Path "C:\temp"}
+<#if (!(Test-Path c:\temp)){New-Item -ItemType Directory -Force -Path "C:\temp"}
 Copy-Item -Path c:\OSDCloud\Logs\*.log -Destination c:\temp\ -Force
 Remove-Item -Path C:\OSDCloud\ -Recurse
 Remove-Item -Path C:\Drivers\ -Recurse
 
 wpeutil reboot
-
+#>
 
