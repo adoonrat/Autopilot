ed569475da29e9225b27db01e1ea0f4a4a922976
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-16 16:45:26 +0700

    Update A05_Apply_Drivers.ps1

--- A05_Apply_Drivers.ps1
+++ A05_Apply_Drivers.ps1
@@ -1,3 +1,28 @@
+#Functions
+function Write-Log {
+
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+        [switch]$fail
+    )
+	
+    If ((Test-Path $LogFilePath) -eq $false) {
+        mkdir $LogFilePath
+    }
+	
+    $time = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    $time + '...' + $Message | Out-File -FilePath $logfilename -Append
+    if ($fail) {
+        Write-Host $Message -ForegroundColor Red
+    }
+    else {
+        Write-Host $Message
+    }
+
+}
+
+
 #Apply Drivers
 
     try {
