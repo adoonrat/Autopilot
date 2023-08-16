2c5643f3d29a16145bbcaa679af2cb14b1bdcb16
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-16 16:35:14 +0700

    Update A04_Apply_OS.ps1

--- A04_Apply_OS.ps1
+++ A04_Apply_OS.ps1
@@ -7,6 +7,29 @@ $logfilename = "$LogFilePath\$date" + "_ImageApply.log"
 $data = (get-volume | Where FileSystemLabel -eq "DATA").DriveLetter + ":"
 $boot = (get-volume | Where FileSystemLabel -eq "BOOT").DriveLetter + ":"
 
+#Functions
+function Write-Log {
+
+    Param (
+        [Parameter(Mandatory = $true)]
+        [string]$Message,
+        [switch]$fail
+    )
+	
+    If ((Test-Path $LogFilePath) -eq $false) {
+        mkdir $LogFilePath
+    }
+	
+    $time = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    $time + '...' + $Message | Out-File -FilePath $logfilename -Append
+    if ($fail) {
+        Write-Host $Message -ForegroundColor Red
+    }
+    else {
+        Write-Host $Message
+    }
+
+}
 #Apply Image
 
 $imagefolder = ls "$data\OS"
