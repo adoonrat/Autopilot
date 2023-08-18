63475da7f31a636e05e67b64fe50294cfb8971f0
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-18 11:06:16 +0700

    Update 00_BIOSSettings.ps1

--- 00_BIOSSettings.ps1
+++ 00_BIOSSettings.ps1
@@ -1,1 +1,24 @@
+Import-Module DellBIOSProvider
+Add-Type -AssemblyName PresentationFramework
+$SataMode = dir DellSmbios:\SystemConfiguration\EmbSataRaid | Select -ExpandProperty CurrentValue
+
+function BIOSSetingMsgbox ()
+{
+    
+  $msgBoxInput =  [System.Windows.MessageBox]::Show('Sata mode is not set to Ahci, please review BIOS settings before attempting to deploy. Press OK to restart.', 'Confirmation', 'OK','Error')
+
+  switch  ($msgBoxInput) {
+
+      'OK' {
+          	## Action Here
+            Write-Host "Sata mode not set to Ahci, review all BIOS settings. Rebooting...."
+	          wpeutil reboot
+            # Exit 69
+          }
+    }
+
+}
+
+If ($SataMode -ne "Ahci")
+    {BIOSSetingMsgbox}
 
