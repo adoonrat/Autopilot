370562f445f707db255d73d0812225b626aeae10
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-21 10:32:58 +0700

    Update A06_WinRE.ps1

--- A06_WinRE.ps1
+++ A06_WinRE.ps1
@@ -25,7 +25,60 @@ catch {
 Write-Log "Copying Unattend.xml to c:\windows\system32\sysprep"
 $unattend = "$boot\unattend.xml"
 try {
-      Copy-Item -Path $unattend -Destination "W:\windows\system32\sysprep" -Force -ErrorAction Stop
+      $Unattended = [xml]@"
+<?xml version="1.0" encoding="utf-8"?>
+<!--Version 2.3-->
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="windowsPE">
+        <component name="Microsoft-Windows-International-Core-WinPE" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <SetupUILanguage>
+                <UILanguage>en-US</UILanguage>
+            </SetupUILanguage>
+            <InputLocale>en-US</InputLocale>
+            <SystemLocale>en-US</SystemLocale>
+            <UILanguage>en-US</UILanguage>
+            <UserLocale>en-US</UserLocale>
+        </component>
+    </settings>
+    <settings pass="specialize">
+        <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <ComputerName>*</ComputerName>
+            <RegisteredOrganization>Amadeus DTS</RegisteredOrganization>
+            <RegisteredOwner>Amadeus DTS</RegisteredOwner>
+            <WindowsFeatures>
+                <ShowInternetExplorer>false</ShowInternetExplorer>
+            </WindowsFeatures>
+        </component>
+        <component name="Microsoft-Windows-Deployment" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            <RunSynchronous>
+                <RunSynchronousCommand wcm:action="add">
+                    <Description>UnfilterAdminToken</Description>
+                    <Path>cmd /c reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v FilterAdministratorToken /t REG_DWORD /d 0 /f</Path>
+                    <Order>1</Order>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Description>Disable consumer features</Description>
+                    <Path>reg add HKLM\Software\Policies\Microsoft\Windows\CloudContent /v DisableWindowsConsumerFeatures /t REG_DWORD /d 1 /f</Path>
+                    <Order>2</Order>
+                </RunSynchronousCommand>
+                <RunSynchronousCommand wcm:action="add">
+                    <Description>DISABLE_UAC_EnableLUA</Description>
+                    <Path>cmd /c reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f</Path>
+                    <Order>3</Order>
+                </RunSynchronousCommand>
+                <!--RunSynchronousCommand wcm:action="add">
+                    <Path>Dism /online /enable-feature /featurename:NetFX3 /All /Source:C:\Temp\sxs /LimitAccess</Path>
+                    <Order>4</Order>
+                    <Description>DISM .net</Description>
+                </RunSynchronousCommand-->
+            </RunSynchronous>
+        </component>
+    </settings>
+    <cpi:offlineImage cpi:source="wim:c:/temp/sw_dvd9_win_pro_10_21h2.4_64bit_english_pro_ent_edu_n_mlf_x23-08263/sources/install.wim#Windows 10 Enterprise" xmlns:cpi="urn:schemas-microsoft-com:cpi" />
+</unattend>
+"@
+
+$Unattended.Save("w:\windows\system32\sysprep\unattend.xml")
 }
 catch {
     write-log "Ran into an issue: $PSItem" -fail
