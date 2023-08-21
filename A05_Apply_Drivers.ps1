2443cedfa61d786275ea88267f85fcd941c0d507
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-21 22:52:25 +0700

    Update A05_Apply_Drivers.ps1

--- A05_Apply_Drivers.ps1
+++ A05_Apply_Drivers.ps1
@@ -1,43 +1,21 @@
-Add-Type -AssemblyName PresentationCore, PresentationFramework
+Start-Transcript x:\A05_Apply_Drivers.log
+
+#Add-Type -AssemblyName PresentationCore, PresentationFramework
 
 #Variable Section
-$date = (Get-Date).ToString('yyyy-MM-dd')
-$LogFilePath = $env:TEMP
-$logfilename = "$LogFilePath\$date" + "_ImageApply.log"
+#$date = (Get-Date).ToString('yyyy-MM-dd')
+#$LogFilePath = $env:TEMP
+#$logfilename = "$LogFilePath\$date" + "_ImageApply.log"
 
-
-#Functions
-function Write-Log {
-
-    Param (
-        [Parameter(Mandatory = $true)]
-        [string]$Message,
-        [switch]$fail
-    )
-	
-    If ((Test-Path $LogFilePath) -eq $false) {
-        mkdir $LogFilePath
-    }
-	
-    $time = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-    $time + '...' + $Message | Out-File -FilePath $logfilename -Append
-    if ($fail) {
-        Write-Host $Message -ForegroundColor Red
-    }
-    else {
-        Write-Host $Message
-    }
-
-}
 
 
 #Apply Drivers
 
     try {
-        Write-Log "Applying Drivers"
+        Write-host "Applying Drivers"
         dism.exe /image:W:\ /Add-Driver /driver:"W:\Drivers" /recurse
     }
     catch {
-        write-log "Ran into an issue: $PSItem"  -fail
+        write-host "Ran into an issue: $PSItem" 
         exit
     }
