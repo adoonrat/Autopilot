b429d8b68ebc1a355ed69b45aaa4bf874c7b1155
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-30 11:26:23 +0700

    Update A04_Apply_OS.ps1

--- A04_Apply_OS.ps1
+++ A04_Apply_OS.ps1
@@ -26,11 +26,11 @@ $imagefile = $imagefolder.Name
 $imagefile = "$data\OS\" + $imagefile
 
 try {
-    Write-host "Applying Image"
+    Write-host "Applying Image" -ForegroundColor Yellow
     dism /Apply-Image /ImageFile:$imagefile /Index:6 /ApplyDir:W:\
 }
 catch {
-    Write-host "Ran into an issue: $PSItem"
+    Write-host "Ran into an issue: $PSItem" -ForegroundColor Red
     exit
 }
 
@@ -42,31 +42,31 @@ if (!(Test-Path "W:\Temp")) {
 # Copy boot files to the System partition ==
 
 try {
-    Write-host "Copying boot files"
+    Write-host "Copying boot files" -ForegroundColor Yellow
     W:\Windows\System32\bcdboot W:\Windows /s S:
 }
 catch {
-    Write-host "Ran into an issue: $PSItem"
+    Write-host "Ran into an issue: $PSItem" -ForegroundColor Red
     exit
 }
 
 try {
-    write-host "Copying WinRE" 
+    write-host "Copying WinRE" -ForegroundColor Yellow
     md R:\Recovery\WindowsRE
     xcopy /h W:\Windows\System32\Recovery\Winre.wim R:\Recovery\WindowsRE\
 }
 catch {
-    write-host "Ran into an issue: $PSItem"
+    write-host "Ran into an issue: $PSItem" -ForegroundColor Red
     exit
 }
 
 # Register the location of the recovery tools 
 try {
-    write-host "Setting location of recovery tools"
+    write-host "Setting location of recovery tools" -ForegroundColor Yellow
     W:\Windows\System32\Reagentc /Setreimage /Path R:\Recovery\WindowsRE /Target W:\Windows | out-null
 }
 catch {
-    write-host "Ran into an issue: $PSItem"
+    write-host "Ran into an issue: $PSItem" -ForegroundColor Red
     exit
 }
 
