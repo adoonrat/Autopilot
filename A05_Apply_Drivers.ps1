914f325c45f53fe3999ff941b18986f53b0936f3
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-08-30 11:26:51 +0700

    Update A05_Apply_Drivers.ps1

--- A05_Apply_Drivers.ps1
+++ A05_Apply_Drivers.ps1
@@ -1,11 +1,11 @@
 Start-Transcript x:\logs\A05_Apply_Drivers.log
 
     try {
-        Write-host "Applying Drivers"
+        Write-host "Applying Drivers" -ForegroundColor Yellow
         dism.exe /image:W:\ /Add-Driver /driver:"W:\Drivers" /recurse
     }
     catch {
-        write-host "Ran into an issue: $PSItem" 
+        write-host "Ran into an issue: $PSItem" -ForegroundColor Red
         exit
     }
 
