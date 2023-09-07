01cc6b0f9037c2625bcdb072bfa4b04afc2d6617
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-09-07 16:29:32 +0700

    Update A06_Finalization.ps1

--- A06_Finalization.ps1
+++ A06_Finalization.ps1
@@ -4,19 +4,19 @@ Start-Transcript x:\logs\A06_Finalization.log
 If(Test-Path -Path "W:\OS\")
     {
         Write-Host "Clear local OSCache" -ForegroundColor Yellow
-        Remove-Item 'W:\OS\' -Recurse
+        Remove-Item 'W:\OS\' -Recurse -Force
     }
 
 If(Test-Path -Path "W:\Dell")
     {
         Write-Host "Clear local Driver CAB" -ForegroundColor Yellow
-        Remove-Item 'W:\Dell\' -Recurse
+        Remove-Item 'W:\Dell\' -Recurse -Force
     }
 
 If(Test-Path -Path "W:\Drivers")
     {
         Write-Host "Clear local Driver Cache" -ForegroundColor Yellow
-        Remove-Item 'W:\Drivers\' -Recurse
+        Remove-Item 'W:\Drivers\' -Recurse -Force
     }
 
 
