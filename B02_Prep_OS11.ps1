3705a6aa8a9a8cfdc1cfdd2478c56a0416e653a3
adoonrat <140907228+adoonrat@users.noreply.github.com>
2023-12-12 18:07:53 +0700

    Update B02_Prep_OS11.ps1

--- B02_Prep_OS11.ps1
+++ B02_Prep_OS11.ps1
@@ -1,25 +1,34 @@
 Start-Transcript x:\logs\A02_Prep_OS.log
-
+ 
+Add-Type -AssemblyName PresentationFramework
+ 
+function Msgbox ($msg,$Type)
+{
+  $msgBoxInput =  [System.Windows.MessageBox]::Show($msg, 'Notification', 'OK',$Type)
+ 
+}
+ 
+ 
 $FindUSBVolume = Get-Volume | Where FileSystemLabel -eq "DATA"
 	if($FindUSBVolume -ne $null)
- 		{
+		{
    			$FindUSBVolume = ($FindUSBVolume.DriveLetter+":")
       		}
 	Else
- 		{
+		{
 			$FindUSBVolume = $False
    		}
-
+ 
 	if ($FindUSBVolume -ne $False)
- 		{
+		{
 			$data = (get-volume | Where FileSystemLabel -eq "DATA").DriveLetter + ":"
 			$boot = (get-volume | Where FileSystemLabel -eq "BOOT").DriveLetter + ":"
    		}
 	Else
- 		{
+		{
 			$data = "W:"
    		}
-
+ 
 ### Check Remaining space ###
 If($data -ne "W:")
 {
@@ -27,39 +36,33 @@ If($data -ne "W:")
 	If($RemainSpace -lt 2)
 		{
 			Write-Host "USB DATA partition Remaining Space is low, you may encounter issue downloading OS Image or new drivers. Please consider to clear some unused Drivers in folder Dell." -ForegroundColor Red
+       Msgbox("USB DATA partition Remaining Space is low, you may encounter issue downloading OS Image or new drivers. Please consider to clear some unused Drivers in folder Dell.") ("Warning")
   		}
 }
 $OSVersion = 'Windows 11'
 $OSReleaseID = "23H2"
-
-$GetOSInfo = Get-FeatureUpdate -OSVersion $OSVersion -OSReleaseID $OSReleaseID -OSArchitecture x64 -OSActivation Volume -OSLanguage en-us #| Select-Object -Property CreationDate,KBNumber,Title,UpdateOS,UpdateBuild,UpdateArch,FileName, @{Name='SizeMB';Expression={[int]($_.Size /1024/1024)}},FileUri,Hash,AdditionalHash
-
+ 
+$GetOSInfo = Get-FeatureUpdate -OSVersion $OSVersion -OSReleaseID $OSReleaseID -OSArchitecture x64 -OSActivation Volume -OSLanguage en-us
+ 
 $FileUri = $GetOSInfo.Url
 $FileName = $GetOSInfo.FileName
-
-
+ 
+ 
 $OSCacheLocation = "$data\OS\"
 If(!(Test-Path -Path $OSCacheLocation))
     {
         Mkdir $OSCacheLocation
     }
- 
 
+ 
 ###Check OS file version####
 $OSCache = ls $OSCacheLocation
-
+ 
 If($FileName -eq $OSCache.Name)
     {
-       	$CacheSize = ($OSCache.Length)/1MB
-	$CacheSize = [math]::Round($CacheSize)
- 	$CloudSize = $GetOSInfo.SizeMB
-	  	
-   	If($CacheSize -gt $CloudSize)
-   		{$Dif = $CacheSize - $CloudSize}
-    	Else
-     		{$Dif = $CloudSize - $CacheSize}
-    	
-     	If($Dif -lt 1)
+	   $onlieHash = $GetOSInfo.SHA1.ToUpper()
+       $localHash = (Get-FileHash -Path "$OSCacheLocation\$OSCache" -Algorithm SHA1).Hash.ToUpper()
+     	If($onlieHash -eq $localHash)
     	{
      		Write-Host "Cache Image is a good version" -ForegroundColor Green
        	}
@@ -69,7 +72,7 @@ If($FileName -eq $OSCache.Name)
         	Remove-Item -Path $OSCacheLocation -Recurse -Force
         	Write-Host "Download Latest image" -ForegroundColor Yellow
         	Save-WebFile -SourceUrl $FileUri -DestinationDirectory $OSCacheLocation -DestinationName $FileName -ErrorAction Stop
-	 }
+   	}
     }
 Else
     {
@@ -77,7 +80,4 @@ Else
 	Remove-Item -Path $OSCacheLocation -Recurse -Force
 	Write-Host "Download Latest image" -ForegroundColor Yellow
 	Save-WebFile -SourceUrl $FileUri -DestinationDirectory $OSCacheLocation -DestinationName $FileName -ErrorAction Stop
- 	
    	}
-    
-    
